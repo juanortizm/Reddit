1d1532efc0a3487573a865967ff9402a1f7491b8
juan.ortiz <juan.ortiz@despegar.com>
2019-04-20 13:24:13 -0300

    Test Parser with mock

--- RedditTests/RedditTests.swift
+++ RedditTests/RedditTests.swift
@@ -11,24 +11,24 @@ import XCTest
 
 class RedditTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testParser() {
+        guard let url = Bundle(for: RedditTests.self).url(forResource: "top", withExtension: "json") else {
+            XCTFail()
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let model = try decoder.decode(RedditEntriesDto.self, from: data)
+            let firstEntry: RedditEntryDto? = model.data.children.first?.data
+            XCTAssertEqual(model.data.children.count, 25)
+            XCTAssertEqual(firstEntry?.id, "2hqlxp")
+            XCTAssertEqual(firstEntry?.author, "washedupwornout")
+            XCTAssertEqual(firstEntry?.thumbnail, "http://b.thumbs.redditmedia.com/9N1f7UGKM5fPZydrsgbb4_SUyyLW7A27um1VOygY3LM.jpg")
+            XCTAssertEqual(firstEntry?.numComments, 958)
+        } catch {
+            XCTFail()
         }
     }
-
 }
