03d35ae55b88eeae97e3c8f8a080feeb8f162f68
juan.ortiz <juan.ortiz@despegar.com>
2019-04-20 15:29:45 -0300

    Test for date time ago builder

--- RedditTests/RedditTests.swift
+++ RedditTests/RedditTests.swift
@@ -31,4 +31,23 @@ class RedditTests: XCTestCase {
             XCTFail()
         }
     }
+    
+    func testDateTimeAgo() {
+        let calendar = Calendar.current
+        let oneDayAgo = calendar.date(byAdding: .day, value: -1, to: Date())!
+        let twoDaysAgo = calendar.date(byAdding: .day, value: -2, to: Date())!
+        let oneHourAgo = calendar.date(byAdding: .hour, value: -1, to: Date())!
+        let twoHoursAgo = calendar.date(byAdding: .hour, value: -2, to: Date())!
+        let oneMinuteAgo = calendar.date(byAdding: .minute, value: -1, to: Date())!
+        let tenMinutesAgo = calendar.date(byAdding: .minute, value: -10, to: Date())!
+        let threeSecondsAgo = calendar.date(byAdding: .second, value: -3, to: Date())!
+        
+        XCTAssertEqual(oneDayAgo.timeAgo(), "1 day ago")
+        XCTAssertEqual(twoDaysAgo.timeAgo(), "2 days ago")
+        XCTAssertEqual(oneHourAgo.timeAgo(), "1 hour ago")
+        XCTAssertEqual(twoHoursAgo.timeAgo(), "2 hours ago")
+        XCTAssertEqual(tenMinutesAgo.timeAgo(), "10 minutes ago")
+        XCTAssertEqual(oneMinuteAgo.timeAgo(), "1 minute ago")
+        XCTAssertEqual(threeSecondsAgo.timeAgo(), "just now")
+    }
 }
